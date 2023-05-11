ebe90a08cccdd6af68f4c5f97edc025612db7793
Peterksharma <120028593+Peterksharma@users.noreply.github.com>
2023-05-11 15:13:18 -0500

    new background

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,8 @@
 
 
 //Tracks the mouse movement across the screen and logs the value
-const parrallax_effect = document.querySelectorAll(".movement")
+const parrallax_effectl = document.querySelectorAll(".movement")
+const parrallax_effectr = document.querySelectorAll(".movementr")
 
 let xValue = 0,
     yValue = 0;
@@ -26,10 +27,15 @@ window.addEventListener('mousemove', (e) => {
     xValue = e.clientX - window.innerWidth / 2;
     yValue = e.clientY - window.innerHeight / 2;
 
-
-    parrallax_effect.forEach((el) => {
+    parrallax_effectl.forEach((el) => {
         let speedx = el.dataset.speedx;
         let speedy = el.dataset.speedy;
         el.style.transform = `translateX(calc(-5% + ${-xValue * speedx}px)) translateY(calc(-5% + ${-yValue * speedy}px))`;
+    })
+
+    parrallax_effectr.forEach((el) => {
+        let speedx1 = el.dataset.speedx1;
+        let speedy1 = el.dataset.speedy1;
+        el.style.transform = `translateX(calc(5% + ${xValue * speedx1}px)) translateY(calc(5% + ${yValue * speedy1}px))`;
     })
 })
