8aa5120e2987423735f53be91eab2d6cca9708ee
Peterksharma <120028593+Peterksharma@users.noreply.github.com>
2023-05-22 18:25:13 -0500

    add hamburger button animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 //Tracks the mouse movement across the screen and logs the value
 const parrallax_effectl = document.querySelectorAll(".movement")
 const parrallax_effectr = document.querySelectorAll(".movementr")
-
+const hamburgerbtn = document.getElementById('hamburgerbtn')
+const navigation = document.querySelector('.navigation')
 let xValue = 0,
     yValue = 0;
 
@@ -18,6 +19,25 @@ window.addEventListener('mousemove', (e) => {
     parrallax_effectr.forEach((el) => {
         let speedx1 = el.dataset.speedx1;
         let speedy1 = el.dataset.speedy1;
-        el.style.transform = `translateX(calc(5% + ${xValue * speedx1}px)) translateY(calc(5% + ${yValue * speedy1}px))`;
+        el.style.transform = `translateX(calc(${(xValue - (window.innerWidth / 2)) * speedx1}px)) translateY(calc(${(yValue - (window.innerHeight / 2)) * speedy1}px))`;
     })
 })
+
+function hideShowHamburgerBtn() {
+    if (navigation.classList.contains('hidden')) {
+        navigation.classList.remove('hidden')
+    } else {
+        navigation.classList.add('hidden')
+    }
+}
+
+// hamburgerbtn.addEventListener('click', hideShowHamburgerBtn);
+
+hamburgerbtn.addEventListener('click', function() {
+    navigation.classList.toggle('hidden');
+    if (navigation.classList.contains('hidden')) {
+        navigation.classList.add('fade-out');
+    } else {
+        navigation.classList.remove('fade-out');
+    }
+});
