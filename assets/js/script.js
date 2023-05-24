75b0f4c46e377cc650f71d0bffbea5a9c225de81
Peterksharma <120028593+Peterksharma@users.noreply.github.com>
2023-05-23 22:33:07 -0500

    updated cursor and fade flow for when links in nav are clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,24 @@
 //Tracks the mouse movement across the screen and logs the value
 const parrallax_effectl = document.querySelectorAll(".movement")
 const parrallax_effectr = document.querySelectorAll(".movementr")
+//Name + Title
+const nameTitle = document.querySelector(".about-me-name")
+//Navigation
 const hamburgerbtn = document.getElementById('hamburgerbtn')
 const navigation = document.querySelector('.navigation')
+//Navigation Links
+const aboutMeLink = document.getElementById('aboutMeLink')
+const projectsLink = document.getElementById('projectsLink')
+const techStackLink = document.getElementById('techStackLink')
+const resumeLink = document.getElementById('resumeLink')
+const contactMeLink = document.getElementById('contactMelink')
+
+const mainContent = document.querySelectorAll('.mainContent')
+const aboutMeContent = document.querySelector('aboutMe')
+
+const mainBox = document.querySelector('.displayBoxMain')
+
+//Parallax Effect
 let xValue = 0,
     yValue = 0;
 
@@ -23,16 +39,10 @@ window.addEventListener('mousemove', (e) => {
     })
 })
 
-function hideShowHamburgerBtn() {
-    if (navigation.classList.contains('hidden')) {
-        navigation.classList.remove('hidden')
-    } else {
-        navigation.classList.add('hidden')
-    }
-}
+//Navigation
 
-// hamburgerbtn.addEventListener('click', hideShowHamburgerBtn);
 
+//Fade-out Effect 
 hamburgerbtn.addEventListener('click', function() {
     navigation.classList.toggle('hidden');
     if (navigation.classList.contains('hidden')) {
@@ -41,3 +51,41 @@ hamburgerbtn.addEventListener('click', function() {
         navigation.classList.remove('fade-out');
     }
 });
+
+//Hiding the nameTitle
+//About Me
+aboutMeLink.addEventListener('click', function() {
+    if (!nameTitle.classList.contains('hidden')) {
+        nameTitle.classList.add('hidden');
+        setTimeout(function(){
+            mainBox.style.animation = "none";
+            mainBox.offsetHeight; //Trigggers a reflow, flushing the CSS elements
+            mainBox.classList.remove('hidden');
+            mainBox.style.animation = "fadeInBox 2s forwards"
+        }, 1000);
+    } 
+})
+//Projects
+projectsLink.addEventListener('click', function() {
+    if (!nameTitle.classList.contains('hidden')) {
+        nameTitle.classList.add('hidden');
+    } 
+})
+//Tech Stack
+techStackLink.addEventListener('click', function() {
+    if (!nameTitle.classList.contains('hidden')) {
+        nameTitle.classList.add('hidden');
+    } 
+})
+//Resume
+resumeLink.addEventListener('click', function() {
+    if (!nameTitle.classList.contains('hidden')) {
+        nameTitle.classList.add('hidden');
+    } 
+})
+//Contact Me
+contactMeLink.addEventListener('click', function() {
+    if (!nameTitle.classList.contains('hidden')) {
+        nameTitle.classList.add('hidden');
+    } 
+})
